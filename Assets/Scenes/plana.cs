ea61435a7a171b3aa728fefb0e96c0c5b6cd6921
渡辺　昌 <maasa1221@gmail.com>
2020-01-09 15:39:58 +0900

    inint and bgm add

---
+++ Assets/Scenes/plana.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plana : MonoBehaviour
+{   
+    AudioSource m_MyAudioSource;
+    float m_MySliderValue;
+    void Start()
+    {
+        //Initiate the Slider value to half way
+        m_MySliderValue = 0.1f;
+        //Fetch the AudioSource from the GameObject
+        m_MyAudioSource = GetComponent<AudioSource>();
+        //Play the AudioClip attached to the AudioSource on startup
+        m_MyAudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_MyAudioSource.volume = m_MySliderValue;
+    }
+}
+
+
+   
